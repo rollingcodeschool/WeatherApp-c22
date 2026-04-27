eea5ef6e3c409dc7bab4bb9d744235e7e4e8f58b
Emilse Arias <emilsearias@gmail.com>
2026-04-27 16:25:53 -0300

    add name to the form elements

--- src/components/FormWeather.jsx
+++ src/components/FormWeather.jsx
@@ -3,10 +3,11 @@ const FormWeather = () => {
     <form className="flex flex-col md:flex-row gap-4 justify-between items-center mt-8">
       <input
         type="text"
+        name='city'
         placeholder="Ej: Madrid"
         className="bg-slate-800 rounded px-4 py-2 text-white focus:outline-none border border-slate-700 w-full"
       />
-      <select className="bg-slate-800 rounded px-4 py-2 text-white border border-slate-700 w-full">
+      <select name='country code' className="bg-slate-800 rounded px-4 py-2 text-white border border-slate-700 w-full">
         <option>España</option>
       </select>
       <button className="bg-blue-600 hover:bg-blue-700 text-white px-6 py-2 rounded">
