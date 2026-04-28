c265751c1510aeae99df2b0c2565340ccca1810b
Emilse Arias <emilsearias@gmail.com>
2026-04-27 21:36:53 -0300

    agregamos el endpoint a fetch y obtuvimos una promise pending

--- src/components/FormWeather.jsx
+++ src/components/FormWeather.jsx
@@ -1,29 +1,51 @@
+import { useForm } from "react-hook-form";
 import WeatherCard from "./WeatherCard";
 import WeatherStats from "./WeatherStats";
 const FormWeather = () => {
+ const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm()
+
+  const buscarClima = (data)=>{
+    console.log(data)
+    consultarAPI()
+  }
+  const api = import.meta.env.VITE_WEATHER_API_KEY
+
+ 
+  const consultarAPI = ()=>{
+    // https://api.openweathermap.org/data/2.5/weather?q=San%20Miguel%20de%20Tucum%C3%A1n,AR&appid={APIKEY}&units=metric&lang=es
+     const response = fetch(`https://api.openweathermap.org/data/2.5/weather?q=San%20Miguel%20de%20Tucum%C3%A1n,AR&appid=${api}&units=metric&lang=es`)
+     console.log(response)
+  }
+
   return (
     <>
-      <form className="flex flex-col md:flex-row gap-4 justify-between items-center mt-8">
+      <form className="flex flex-col md:flex-row gap-4 justify-between items-center mt-8" onSubmit={handleSubmit(buscarClima)}>
         <div className=" w-full">
           <input
             type="text"
             name="city"
             placeholder="Ej: Madrid"
             className="bg-slate-800 rounded px-4 py-2 text-white focus:outline-none border border-slate-700 w-full"
+            {...register('city', {required: 'La ciudad es obligatoria'})}
           />
-          <p className="text-sm text-red-500"></p>
+          <p className="text-sm text-red-500">{errors.city?.message}</p>
         </div>
         <div className=" w-full">
           <select
             name="country code"
             className="bg-slate-800 rounded px-4 py-2 text-white border border-slate-700 w-full"
+              {...register('country', {required: 'El pais es obligatorio'})}
           >
             <option value="ES">España</option>
             <option value="AR">Argentina</option>
             <option value="MX">Mexico</option>
             <option value="US">Estados Unidos</option>
           </select>
-          <p className="text-sm text-red-500"></p>
+          <p className="text-sm text-red-500">{errors.country?.message}</p>
         </div>
         <button className="bg-blue-600 hover:bg-blue-700 text-white px-6 py-2 rounded">
           Buscar
