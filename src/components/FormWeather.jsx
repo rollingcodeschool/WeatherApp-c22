f1e4f126df2f9c6bd1acd0b35d40d9933f1890d4
Emilse Arias <emilsearias@gmail.com>
2026-04-29 20:07:02 -0300

    realizamos la solicitud a la api en un try catch y la respuesta la guardamos en un state

--- src/components/FormWeather.jsx
+++ src/components/FormWeather.jsx
@@ -1,36 +1,55 @@
 import { useForm } from "react-hook-form";
 import WeatherCard from "./WeatherCard";
 import WeatherStats from "./WeatherStats";
+import { useState } from "react";
 const FormWeather = () => {
- const {
+  const {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm()
+  } = useForm();
+  const [climaData, setClimaData] = useState({});
 
-  const buscarClima = (data)=>{
-    console.log(data)
-    consultarAPI()
-  }
-  const api = import.meta.env.VITE_WEATHER_API_KEY
+  const buscarClima = (data) => {
+    console.log(data);
+    consultarAPI(data.city, data.country);
+  };
+  const api = import.meta.env.VITE_WEATHER_API_KEY;
 
- 
-  const consultarAPI = ()=>{
-    // https://api.openweathermap.org/data/2.5/weather?q=San%20Miguel%20de%20Tucum%C3%A1n,AR&appid={APIKEY}&units=metric&lang=es
-     const response = fetch(`https://api.openweathermap.org/data/2.5/weather?q=San%20Miguel%20de%20Tucum%C3%A1n,AR&appid=${api}&units=metric&lang=es`)
-     console.log(response)
-  }
+  const consultarAPI = async (city, country) => {
+    try {
+      // https://api.openweathermap.org/data/2.5/weather?q=San%20Miguel%20de%20Tucum%C3%A1n,AR&appid={APIKEY}&units=metric&lang=es
+      const response = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${api}&units=metric&lang=es`,
+      );
+      if (response.ok) {
+        const clima = await response.json();
+        console.log(clima);
+        setClimaData(clima);
+      } else {
+        alert("ocurrio un error");
+        setClimaData({});
+      }
+      console.log(response);
+    } catch (error) {
+      console.error(error)
+      alert('Ocurrio un error, vuelve a intentarlo en unos minutos')
+    }
+  };
 
   return (
     <>
-      <form className="flex flex-col md:flex-row gap-4 justify-between items-center mt-8" onSubmit={handleSubmit(buscarClima)}>
+      <form
+        className="flex flex-col md:flex-row gap-4 justify-between items-center mt-8"
+        onSubmit={handleSubmit(buscarClima)}
+      >
         <div className=" w-full">
           <input
             type="text"
             name="city"
             placeholder="Ej: Madrid"
             className="bg-slate-800 rounded px-4 py-2 text-white focus:outline-none border border-slate-700 w-full"
-            {...register('city', {required: 'La ciudad es obligatoria'})}
+            {...register("city", { required: "La ciudad es obligatoria" })}
           />
           <p className="text-sm text-red-500">{errors.city?.message}</p>
         </div>
@@ -38,7 +57,7 @@ const FormWeather = () => {
           <select
             name="country code"
             className="bg-slate-800 rounded px-4 py-2 text-white border border-slate-700 w-full"
-              {...register('country', {required: 'El pais es obligatorio'})}
+            {...register("country", { required: "El pais es obligatorio" })}
           >
             <option value="ES">España</option>
             <option value="AR">Argentina</option>
